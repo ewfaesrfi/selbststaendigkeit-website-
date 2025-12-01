94cf1a25ce6800a35c708b9caf992251dda6d264
BrauDirDeinBusiness <cdxgx8fhsx@privaterelay.appleid.com>
2025-12-01 23:11:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ toggle.addEventListener("click", () => {
   body.classList.toggle("light");
   body.classList.toggle("dark");
 });
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
